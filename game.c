05eb6100f1adc975ec5dbd59c00879676fa1341c
Eric Sundell <e.j.sundell@gmail.com>
2020-10-03 13:39:56 -0400

    Add collision detection for edges

--- game.c
+++ game.c
@@ -7,13 +7,19 @@ const short player_x_coords[PLAYER_COUNT] = {8, TABLE_WIDTH - 8 - PADDLE_WIDTH};
 
 static void move_ball(struct GameState *state);
 
+static void reset_ball(struct Ball *ball, int dir_x);
+
+static void inc_score(unsigned char *score)
+{
+    unsigned new_score = *score + 1;
+    if (new_score >= 100)
+        new_score = 99;
+    *score = new_score;
+}
+
 void g_init(struct GameState *state)
 {
-    state->ball.x_coord = coord_from_int(TABLE_WIDTH / 2 - BALL_SIZE / 2);
-    state->ball.y_coord = coord_from_int(10);
-    state->ball.dir_x = 1;
-    state->ball.dir_y = 1;
-    state->ball.speed = 1;
+    reset_ball(&(state->ball), -1);
 
     for (size_t i = 0; i < PLAYER_COUNT; ++i)
     {
@@ -43,6 +49,48 @@ static void move_ball(struct GameState *state)
     int denom = abs(state->ball.dir_x) + abs(state->ball.dir_y);
     int vel_x = coord_mul_frac(speed, state->ball.dir_x, denom);
     int vel_y = coord_mul_frac(speed, state->ball.dir_y, denom);
-    state->ball.x_coord += vel_x;
-    state->ball.y_coord += vel_y;
+    int new_x = state->ball.x_coord + vel_x;
+    int new_y = state->ball.y_coord + vel_y;
+
+    // Left edge collision
+    if (new_x < 0)
+    {
+        inc_score(&(state->players[1].score));
+        reset_ball(&(state->ball), 1);
+        return;
+    }
+
+    // Right edge collision
+    if (coord_to_int(new_x) + BALL_SIZE > TABLE_WIDTH)
+    {
+        inc_score(&(state->players[0].score));
+        reset_ball(&(state->ball), -1);
+        return;
+    }
+
+    // Top edge collision
+    if (new_y < 0)
+    {
+        state->ball.dir_y = -state->ball.dir_y;
+        new_y = 0;
+    }
+
+    // Bottom edge collision
+    if (coord_to_int(new_y) + BALL_SIZE > TABLE_HEIGHT)
+    {
+        state->ball.dir_y = -state->ball.dir_y;
+        new_y = coord_from_int(TABLE_HEIGHT - BALL_SIZE);
+    }
+
+    state->ball.x_coord = new_x;
+    state->ball.y_coord = new_y;
+}
+
+static void reset_ball(struct Ball *ball, int dir_x)
+{
+    ball->x_coord = coord_from_int(TABLE_WIDTH / 2 - BALL_SIZE / 2);
+    ball->y_coord = coord_from_int(10);
+    ball->dir_x = dir_x;
+    ball->dir_y = 1;
+    ball->speed = 1;
 }
