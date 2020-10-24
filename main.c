549b513eed6f8320ecde3ad5b404eaa2b80ead35
Eric Sundell <e.j.sundell@gmail.com>
2020-10-24 12:41:56 -0400

    Parse program arguments

--- main.c
+++ main.c
@@ -5,8 +5,49 @@
 #include <SDL.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FRAME_TIME (1000/60)
+
+static bool p1_ai = false;
+static bool p2_ai = false;
+
+static const char * const help_text =
+"Options:\n"
+"--ai1\t\tLeft player is controlled by AI\n"
+"--ai2\t\tRight player is controlled by AI\n"
+"--easy\t\tSets the AI difficulty to easy\n"
+"--normal\tSets the AI difficulty to normal\n"
+"--hard\t\tSets the AI difficulty to hard";
+
+static void parse_args(int argc, char **argv)
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        if (strcmp(argv[i], "--ai1") == 0)
+            p1_ai = true;
+        else if (strcmp(argv[i], "--ai2") == 0)
+            p2_ai = true;
+        else if (strcmp(argv[i], "--easy") == 0)
+            ai_set_difficulty(AI_EASY);
+        else if (strcmp(argv[i], "--normal") == 0)
+            ai_set_difficulty(AI_NORMAL);
+        else if (strcmp(argv[i], "--hard") == 0)
+            ai_set_difficulty(AI_HARD);
+        else if (strcmp(argv[i], "--help") == 0)
+        {
+            puts(help_text);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            fprintf(stderr, "%s: Unrecognized option '%s'\n", argv[0], argv[i]);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
 static void read_inputs(PlayerInput *inputs)
 {
@@ -49,7 +90,10 @@ static bool main_loop(void)
         last_frame = current_frame;
         PlayerInput inputs[PLAYER_COUNT] = {0};
         read_inputs(inputs);
-        inputs[1] = ai_determine_input(&game_state, 1);
+        if (p1_ai)
+            inputs[0] = ai_determine_input(&game_state, 0);
+        if (p2_ai)
+            inputs[1] = ai_determine_input(&game_state, 1);
         while (remaining_time >= FRAME_TIME)
         {
             g_update(&game_state, inputs);
@@ -63,6 +107,8 @@ static bool main_loop(void)
 
 int main(int argc, char **argv)
 {
+    parse_args(argc, argv);
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         u_display_sdl_error();
