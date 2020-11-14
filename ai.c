526084f81c69453494fb7e681f539819543a6170
Eric Sundell <e.j.sundell@gmail.com>
2020-11-13 20:24:11 -0500

    Bias AI to prevent stalemates

--- ai.c
+++ ai.c
@@ -12,7 +12,7 @@ PlayerInput ai_determine_input(const struct GameState *state, size_t player_inde
         coord_to_int(state->ball.x_coord) + BALL_SIZE / 2
         - (player_x_coords[player_index] + PADDLE_WIDTH / 2));
     int y_diff = coord_to_int(state->ball.y_coord) + BALL_SIZE / 2
-        - (state->players[player_index].y + PADDLE_HEIGHT / 2);
+        - (state->players[player_index].y + PADDLE_HEIGHT / 2 - 1);
     int dir = y_diff;
     x_dist /= difficulty;
     if (x_dist > 0)
